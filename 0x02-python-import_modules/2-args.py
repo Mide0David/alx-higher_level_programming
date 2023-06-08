7c9f746a0285d65c4e99ab3922614226cb6f8b22
mide <midebabalola7@gmail.com>
2023-06-08 21:10:27 +0000

    print num of the arguments list

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
 
-    print("{} argument{}:".format(num_arguments, "s" if num_arguments != 1 else ""), end=' ')
-    print("." if num_arguments == 0 else "")
+    print("{} argument".format(num_arguments), end='')
+    print("{}:".format('s' if num_arguments != 1 else ''))
+    print(".\n" if num_arguments == 0 else "")
 
     for i, arg in enumerate(arguments, start=1):
         print("{}: {}".format(i, arg))
