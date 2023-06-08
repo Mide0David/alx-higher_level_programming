cf13fca7b29e76273d59240a2e96e06b10b48de1
mide <midebabalola7@gmail.com>
2023-06-08 21:25:51 +0000

    prints all the names of defined by hidden_4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
-    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    module_names.sort()
+    mod_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    mod_names.sort()
 
-    for name in module_names:
+    for name in mod_names:
         print(name)
