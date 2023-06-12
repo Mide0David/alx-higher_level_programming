95580b7614dc6799c7c81ab07eb812809efbea03
mide <midebabalola7@gmail.com>
2023-06-12 11:30:42 +0000

    retrieves an element form a index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    length = len(my_list)
+    if idx < 0:
+        return None
+    if idx > length:
+        return None
+    for i in my_list:
+        if i == idx:
+            return my_list[i]
